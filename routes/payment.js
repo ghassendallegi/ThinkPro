b15f4247f8efb85dc2f292ac3c52640420668e11
ghassendallagi <ghassen.dallegi.8@gmail.com>
2024-05-31 23:07:21 +0100

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -23,5 +23,50 @@ router.post('/create-payment-intent', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+router.post('/update-payment-intent', async (req, res) => {
+    const { paymentIntentId, paymentMethodId } = req.body;
+  
+    try {
+      const paymentIntent = await stripe.paymentIntents.update(paymentIntentId, {
+        payment_method: paymentMethodId,
+      });
+      res.send(paymentIntent);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+router.post('/confirm-payment-intent', async (req, res) => {
+  const { paymentIntentId } = req.body;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.confirm(paymentIntentId);
+    res.send(paymentIntent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/cancel-payment-intent', async (req, res) => {
+  const { paymentIntentId } = req.body;
+
+  try {
+    const canceledPaymentIntent = await stripe.paymentIntents.cancel(paymentIntentId);
+    res.send(canceledPaymentIntent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/get-payment-intent/:id', async (req, res) => {
+  const paymentIntentId = req.params.id;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId);
+    res.send(paymentIntent);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 export default router;
